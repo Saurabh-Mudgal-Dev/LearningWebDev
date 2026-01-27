2097b53f9022f823038280e0a4141431feab994c
Saurabh-Mudgal-Dev <saurabhmudgl2310@gmail.com>
2026-01-27 22:24:13 +0530

    basic string methods

---
+++ Day5/script.js
@@ -0,0 +1,11 @@
+let str = "javascript is fun";
+let sub1 = str.slice(0,10);
+console.log(sub1);
+let sub2 = str.slice(-2, 17);
+console.log(sub2);
+
+// find occurrence of "r" in string
+str = "rrrjbfdjiofioefwbijoerrrrhhr";
+let arr = str.split("r");
+console.log(arr.length - 1);
+
