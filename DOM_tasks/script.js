d1725443eecba67e95e55f3a8e3ca6e8910239e3
Saurabh-Mudgal-Dev <saurabhmudgal2310@gmail.com>
2026-02-03 18:10:17 +0530

    using tagname DOM and alternate element styling logic

--- DOM_tasks/script.js
+++ DOM_tasks/script.js
@@ -4,8 +4,16 @@ task1.style.padding = "15px";
 task1.style.textAlign = "center";
 
 const task2 = document.getElementsByClassName("a");
-for (let i = 0; i < task2.length; i++){
+for (let i = 0; i < task2.length; i++) {
     task2[i].style.color = "red";
     task2[i].style.fontSize = "20px";
 }
 console.log(task2.length);
+
+const task3 = document.getElementsByTagName("p");
+for (let i = 0; i < task3.length - 1; i++) {
+    if (i % 2 === 0) { task3[i].style.color = "blue"; }
+    else { task3[i].style.color = "green" ;}
+}
+task3[task3.length - 1].style.fontWeight = "bold";
+
