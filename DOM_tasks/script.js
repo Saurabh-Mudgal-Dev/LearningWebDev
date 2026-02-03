7c92c9f8e1275c5c5d3fea1943bfd379da5a7b2d
Saurabh-Mudgal-Dev <saurabhmudgal2310@gmail.com>
2026-02-03 18:22:57 +0530

    queryselectorALL update

--- DOM_tasks/script.js
+++ DOM_tasks/script.js
@@ -13,10 +13,17 @@ console.log(task2.length);
 const task3 = document.getElementsByTagName("p");
 for (let i = 0; i < task3.length - 1; i++) {
     if (i % 2 === 0) { task3[i].style.color = "blue"; }
-    else { task3[i].style.color = "green" ;}
+    else { task3[i].style.color = "green"; }
 }
 task3[task3.length - 1].style.fontWeight = "bold";
 
 const task4 = document.querySelector(".box");
 task4.style.backgroundColor = "tomato";
 task4.textContent = "I am first box";
+
+const task5 = document.querySelectorAll(".card");
+for (let i = 0; i < task5.length; i++) {
+    task5[i].style.border = "2px solid gray";
+    task5[i].style.padding = "15px";
+    task5[i].style.borderRadius = "6px";
+}
